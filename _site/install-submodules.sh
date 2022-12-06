71524fa30a6ed170d85fc27f8e539aa4cdcec418
Robert <rcrooks@brightcove.com>
2022-12-06 14:19:33 -0500

    rebuild for new subm

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -5,6 +5,7 @@ cd ../_includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-brightcove-interactivity
+git submodule add https://github.com/BrightcoveLearning/s-template-page-includes
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-support-img
@@ -14,6 +15,7 @@ cd ..
 echo 'Adding site assets'
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-template-stylesheets
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-scripts
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-images
 cd ..
